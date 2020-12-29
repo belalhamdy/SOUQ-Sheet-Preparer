a25872002528f1bfd92244e92115cff459162c9d
Belal Hamdy <belalhezzat@gmail.com>
2020-12-29 12:26:26 +0200

    First output

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,7 +5,7 @@ import org.apache.log4j.Logger;
 public class Main {
     public static void main(String[] args) {
         // Do not forget to change every comma in the uploaded data with dot
-        // TODO: in the next patches replace the TSV file with the one inside SOUQ Excel
+        // TODO: in the next patches replace the TSV file with the one inside SOUQ Exce
 
         String sampleExcel = "S:\\work\\armor\\Skins Project\\SOUQ Excel\\Sample.xlsx";
         String outputPath = "S:\\work\\armor\\Skins Project\\Final\\Excel Out\\Batch 5";
